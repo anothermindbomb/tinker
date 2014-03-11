06ddc5ec9b185ae964c10877511bd5cfc5f67592
anothermindbomb <anothermindbomb@gmail.com>
2014-03-11 16:32:36 +0000

    Initial commit to test vs2010 to vs2013.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Tinker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"C:/trimtest\AllNonStringFields/trimtest.txt";
+            string lineOfData;
+            using (var inputFile = new StreamReader(filename, true))
+            {
+                lineOfData = inputFile.ReadLine();
+                while (lineOfData != null)
+                {
+                    // Console.WriteLine(lineOfData);
+                    lineOfData = inputFile.ReadLine();
+                }
+            }
+
+        }
+    }
+}
