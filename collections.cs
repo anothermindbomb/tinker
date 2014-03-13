04e1bd7b46c983e6b1e9b41a295be04174022421
anothermindbomb <anothermindbomb@gmail.com>
2014-03-13 13:21:21 +0000

    Add an example of adding to  a list

---
+++ collections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace collections
+{
+    class Collections
+    {
+        static void Main(string[] args)
+        {
+            List<string> theBigList = new List<string>();
+            foreach (string directory in Directory.GetDirectories(@"C:/"))
+            {
+                theBigList.Add(directory);
+
+            }
+            theBigList.Sort();
+
+            foreach (var entry in theBigList)
+            {
+                Console.WriteLine(entry);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
